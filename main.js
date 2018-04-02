0e8fb588a7cdac69d5215250594df53f65094d83
Saloni Saraf <ssaraf3@wisc.edu>
2018-04-01 20:50:56 -0500

    Adding data to database

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var firebase = require("firebase");
-  // Initialize Firebase
+// Initialize Firebase
   var config = {
     apiKey: "AIzaSyC57tVt-cUjcQdNGQueq26DBO1jod6sg_0",
     authDomain: "ece453smartstation.firebaseapp.com",
@@ -9,3 +8,48 @@ var firebase = require("firebase");
     messagingSenderId: "272582667547"
   };
   firebase.initializeApp(config);
+
+  //Access the database
+  var db = firebase.database();
+  var sessionsRef = db.ref("sessions");
+
+  //Detect when we receive values for the form filled out by the user
+  sessionsRef.on('value', function(snapshot){
+        vueRoot.sessions = snapshot.val();
+  });
+
+  //Create a Vue instance and store the different fields entered in teh form
+  var vueRoot = new Vue({
+    el: "#root",
+    data:{
+        //Declaration and initilization of fields associated with each work session
+        newSession:{
+            username: '',
+            sessionName: '',
+            sessionDate: '',
+            startTime: '',
+            endTime: '',
+            goals: '',
+            priorityRank: []
+        }, 
+        //Declration and initilization of array that stores all the sessions on workstation
+        sessions: []
+    },
+    //Adding data to database
+    methods:{
+        addNewSession: function(){
+            //Add new session entry into database
+            var newSessionRef = sessionsRef.push();
+            newSessionRef.set(this.newSession);
+            this.newSession = {
+                username: '',
+                sessionName: '',
+                sessionDate: '',
+                startTime: '',
+                endTime: '',
+                goals: '',
+                priorityRank: []               
+            }
+        }
+    }
+  });
